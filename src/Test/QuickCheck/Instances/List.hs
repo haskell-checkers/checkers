133491362a072671f973f72fd2d1a99a524c73e2
David Sankel <david@sankelsoftware.com>
2008-08-18 11:42:32 -0700

    Added increasingInf for an infinitely increasing list.
    darcs-hash:20080818184232-2cfc8-18e0b99319591166260bd199d77db5e17da4224a

--- src/Test/QuickCheck/Instances/List.hs
+++ src/Test/QuickCheck/Instances/List.hs
@@ -2,6 +2,7 @@ module Test.QuickCheck.Instances.List
        (anyList,nonEmpty
        ,infiniteList
        ,increasing,nondecreasing
+       ,increasingInf
        ) where
 
 import Test.QuickCheck
@@ -26,18 +27,25 @@ anyList x = frequency [(1, pure []), (4, nonEmpty x)]
 infiniteList :: Arbitrary a => Gen a -> Gen [a]
 infiniteList x = liftA2 (:) x (infiniteList x)
 
+
 sumA :: (Applicative f, Num a) => f a -> f [a] -> f [a]
 sumA = liftA2 (scanl (+))
 
+monotonic_ :: (Arbitrary a, Num a) => (Gen a -> Gen [a]) -> Gen a -> Gen [a]
+monotonic_ listGen gen = sumA arbitrary (listGen gen)
+
 -- TODO: Generalise this to Ord a.
 monotonic :: (Arbitrary a, Num a) => Gen a -> Gen [a]
-monotonic gen = sumA arbitrary (anyList gen)
+monotonic gen = monotonic_ anyList gen
 
--- | Generate increasing values
+-- | Generate an infinite list of increasing values
+increasingInf :: (Arbitrary a, Num a) => Gen [a]
+increasingInf = monotonic_ infiniteList positive
+
+-- | Generate increasing towards infinity
 increasing :: (Arbitrary a, Num a) => Gen [a]
 increasing = monotonic positive
 
 -- | Generate nondecreasing values
 nondecreasing :: (Arbitrary a, Num a) => Gen [a]
 nondecreasing = monotonic nonNegative
-
