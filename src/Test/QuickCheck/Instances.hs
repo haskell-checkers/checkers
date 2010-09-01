90cbd07b49aec12c67eea2a3112eb473ffea3613
Mitsutoshi Aoe <maoe.maoe@gmail.com>
2010-08-31 21:49:27 -0700

    Updated for QuickCheck 2.3
    Ignore-this: 349d44514fb852c21566c9fb38687868
      - Removed Word instances of Arbitrary and CoArbitrary
      - Removed Int instances of Arbitrary and CoArbitrary
    
    darcs-hash:20100901044927-d9876-79fc321a7c51691074abc507594cd93f7c4fa8bf

--- src/Test/QuickCheck/Instances.hs
+++ src/Test/QuickCheck/Instances.hs
@@ -12,10 +12,8 @@ module Test.QuickCheck.Instances
 import Test.QuickCheck.Instances.Array ()
 import Test.QuickCheck.Instances.Char 
 import Test.QuickCheck.Instances.Eq
-import Test.QuickCheck.Instances.Int   ()
 import Test.QuickCheck.Instances.List
 import Test.QuickCheck.Instances.Maybe
 import Test.QuickCheck.Instances.Num
 import Test.QuickCheck.Instances.Ord
 import Test.QuickCheck.Instances.Tuple
-import Test.QuickCheck.Instances.Word  ()
