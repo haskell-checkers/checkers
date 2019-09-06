e1a8da6c6344b1c690acfa84da72e5f4859be596
Conal Elliott <conal@conal.net>
2019-09-06 08:43:37 -0700

    -Wno-redundant-constraints for GHC >= 8.0

--- src/Test/QuickCheck/Instances/Array.hs
+++ src/Test/QuickCheck/Instances/Array.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
+#if MIN_VERSION_base(4,9,0)
+-- https://github.com/conal/checkers/pull/38
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+#endif
+
 module Test.QuickCheck.Instances.Array where
 
 import Test.QuickCheck
@@ -8,5 +14,5 @@ instance (Ix a, Integral a, Arbitrary b) => Arbitrary (Array a b) where
   arbitrary   =
     (\x -> listArray (0,fromIntegral (length x - 1)) x) <$> arbitrary
 
-instance CoArbitrary b => CoArbitrary (Array a b) where
+instance (Ix a, CoArbitrary b) => CoArbitrary (Array a b) where
   coarbitrary = coarbitrary . elems
