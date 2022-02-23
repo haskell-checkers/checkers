877d6ae000943994d4ea783bcf28541a7055062c
Simon Jakobi <simon.jakobi@gmail.com>
2022-02-23 03:16:32 +0100

    Remove redundant constraint from instance CoArbitrary Array (#65)
    We can do this now that we've dropped support for GHC 7.10
    in #63.

--- src/Test/QuickCheck/Instances/Array.hs
+++ src/Test/QuickCheck/Instances/Array.hs
@@ -1,19 +1,14 @@
 {-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
-#if MIN_VERSION_base(4,9,0)
--- https://github.com/conal/checkers/pull/38
-{-# OPTIONS_GHC -Wno-redundant-constraints #-}
-#endif
 
 module Test.QuickCheck.Instances.Array where
 
 import Test.QuickCheck
 import Data.Array
 
--- The redundant (Ix a) constraint is required with GHC-7.10.
 instance (Ix a, Integral a, Arbitrary b) => Arbitrary (Array a b) where
   arbitrary   =
     (\x -> listArray (0,fromIntegral (length x - 1)) x) <$> arbitrary
 
-instance (Ix a, CoArbitrary b) => CoArbitrary (Array a b) where
+instance (CoArbitrary b) => CoArbitrary (Array a b) where
   coarbitrary = coarbitrary . elems
